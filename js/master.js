bd6c4922d4943ddd13079764f5eb034fa1378413
Alexandre Girard <alx.girard@gmail.com>
2009-11-22 14:11:24 +0100

    correct typo

--- js/master.js
+++ js/master.js
@@ -6,17 +6,17 @@ $('document').ready(function() {
 		timeout: 60000
 	}
 
-	$('#clock-alphaone').jclock($.extend(default_clock_options, {utcOffset: -5});
-	$('#clock-makerslocal256').jclock($.extend(default_clock_options, {utcOffset: -6});
-	$('#clock-familab').jclock($.extend(default_clock_options, {utcOffset: -5});
-	$('#clock-syn2cat').jclock($.extend(default_clock_options, {utcOffset: 1});
-	$('#clock-hive13').jclock($.extend(default_clock_options, {utcOffset: -5});
-	$('#clock-initlab').jclock($.extend(default_clock_options, {utcOffset: 2});
-	$('#clock-tetalab').jclock($.extend(default_clock_options, {utcOffset: 1});
-	$('#clock-tog').jclock($.extend(default_clock_options, {utcOffset: 0});
-	$('#clock-kwartzLab').jclock($.extend(default_clock_options, {utcOffset: -5});
-	$('#clock-pumpingone').jclock($.extend(default_clock_options, {utcOffset: -6});
-	$('#clock-bcn').jclock($.extend(default_clock_options, {utcOffset: 1});
-	$('#clock-cowtown').jclock($.extend(default_clock_options, {utcOffset: -6});
-	$('#clock-freeside').jclock($.extend(default_clock_options, {utcOffset: -5});
+	$('#clock-alphaone').jclock($.extend(default_clock_options, {utcOffset: -5}));
+	$('#clock-makerslocal256').jclock($.extend(default_clock_options, {utcOffset: -6}));
+	$('#clock-familab').jclock($.extend(default_clock_options, {utcOffset: -5}));
+	$('#clock-syn2cat').jclock($.extend(default_clock_options, {utcOffset: 1}));
+	$('#clock-hive13').jclock($.extend(default_clock_options, {utcOffset: -5}));
+	$('#clock-initlab').jclock($.extend(default_clock_options, {utcOffset: 2}));
+	$('#clock-tetalab').jclock($.extend(default_clock_options, {utcOffset: 1}));
+	$('#clock-tog').jclock($.extend(default_clock_options, {utcOffset: 0}));
+	$('#clock-kwartzLab').jclock($.extend(default_clock_options, {utcOffset: -5}));
+	$('#clock-pumpingone').jclock($.extend(default_clock_options, {utcOffset: -6}));
+	$('#clock-bcn').jclock($.extend(default_clock_options, {utcOffset: 1}));
+	$('#clock-cowtown').jclock($.extend(default_clock_options, {utcOffset: -6}));
+	$('#clock-freeside').jclock($.extend(default_clock_options, {utcOffset: -5}));
 });
