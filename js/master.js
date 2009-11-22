c3279170aefea762860c484fd530f2c4b0a3d9e6
Alexandre Girard <alx.girard@gmail.com>
2009-11-22 13:45:07 +0100

    begin to add clock

--- js/master.js
+++ js/master.js
@@ -1,3 +1,67 @@
 $('document').ready(function() {
 
+	$('#clock-alphaone').jclock({
+		utc: true,
+		utcOffset: 5.5
+	});
+
+	$('#clock-makerslocal256').jclock({
+		utc: true,
+		utcOffset: 5.5
+	});
+
+	$('#clock-familab').jclock({
+		utc: true,
+		utcOffset: 5.5
+	});
+
+	$('#clock-syn2cat').jclock({
+		utc: true,
+		utcOffset: 1
+	});
+
+	$('#clock-hive13').jclock({
+		utc: true,
+		utcOffset: 5.5
+	});
+
+	$('#clock-initlab').jclock({
+		utc: true,
+		utcOffset: 5.5
+	});
+
+	$('#clock-tetalab').jclock({
+		utc: true,
+		utcOffset: 1
+	});
+
+	$('#clock-tog').jclock({
+		utc: true,
+		utcOffset: 5.5
+	});
+
+	$('#clock-kwartzLab').jclock({
+		utc: true,
+		utcOffset: 5.5
+	});
+
+	$('#clock-pumpingone').jclock({
+		utc: true,
+		utcOffset: 5.5
+	});
+	
+	$('#clock-bcn').jclock({
+		utc: true,
+		utcOffset: 1
+	});
+	
+	$('#clock-cowtown').jclock({
+		utc: true,
+		utcOffset: 5.5
+	});
+	
+	$('#clock-freeside').jclock({
+		utc: true,
+		utcOffset: 5.5
+	});
 });
