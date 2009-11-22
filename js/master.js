1d7abbc08a228dc59e0c42920225afbad2c7c258
Alexandre Girard <alx.girard@gmail.com>
2009-11-22 15:10:30 +0100

    reorder by utc offset

--- js/master.js
+++ js/master.js
@@ -6,18 +6,18 @@ $('document').ready(function() {
 		utc: true,
 		timeout: 60000
 	}
-
-	$('#clock-alphaone').jclock($.extend(default_clock_options, {utcOffset: -5}));
-	$('#clock-makerslocal256').jclock($.extend(default_clock_options, {utcOffset: -6}));
-	$('#clock-familab').jclock($.extend(default_clock_options, {utcOffset: -5}));
-	$('#clock-syn2cat').jclock($.extend(default_clock_options, {utcOffset: 1}));
-	$('#clock-hive13').jclock($.extend(default_clock_options, {utcOffset: -5}));
+	
 	$('#clock-initlab').jclock($.extend(default_clock_options, {utcOffset: 2}));
+	$('#clock-syn2cat').jclock($.extend(default_clock_options, {utcOffset: 1}));
+	$('#clock-bcn').jclock($.extend(default_clock_options, {utcOffset: 1}));
 	$('#clock-tetalab').jclock($.extend(default_clock_options, {utcOffset: 1}));
 	$('#clock-tog').jclock($.extend(default_clock_options, {utcOffset: 0}));
+	$('#clock-alphaone').jclock($.extend(default_clock_options, {utcOffset: -5}));
+	$('#clock-familab').jclock($.extend(default_clock_options, {utcOffset: -5}));
+	$('#clock-hive13').jclock($.extend(default_clock_options, {utcOffset: -5}));
 	$('#clock-kwartzLab').jclock($.extend(default_clock_options, {utcOffset: -5}));
-	$('#clock-pumpingone').jclock($.extend(default_clock_options, {utcOffset: -6}));
-	$('#clock-bcn').jclock($.extend(default_clock_options, {utcOffset: 1}));
-	$('#clock-cowtown').jclock($.extend(default_clock_options, {utcOffset: -6}));
 	$('#clock-freeside').jclock($.extend(default_clock_options, {utcOffset: -5}));
+	$('#clock-makerslocal256').jclock($.extend(default_clock_options, {utcOffset: -6}));
+	$('#clock-pumpingone').jclock($.extend(default_clock_options, {utcOffset: -6}));
+	$('#clock-cowtown').jclock($.extend(default_clock_options, {utcOffset: -6}));
 });
